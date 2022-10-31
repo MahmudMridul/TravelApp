266c9ea9b8a771f2c966f691f036977bee157ee5
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-10-31 11:31:23 +0600

    Added Spot class

---
+++ API/TravelAppApi/Models/Spot.cs
@@ -0,0 +1,20 @@
+﻿namespace TravelAppApi.Models
+{
+    public class Spot
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public double Rating { get; set; }
+        public string? ImagePath { get; set; }
+        public string Category{ get; set; }
+        public string District { get; set; }
+
+        public Spot(string name, string category, string district)
+        {
+            Name = name;
+            Rating = 0.0;
+            Category = category;
+            District = district;
+        }
+    }
+}
