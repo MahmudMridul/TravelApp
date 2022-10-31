a62724f20223b98f1fe2e5fdd036de812dade283
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-10-31 11:59:22 +0600

    Id added to Spot

--- API/TravelAppApi/Models/Spot.cs
+++ API/TravelAppApi/Models/Spot.cs
@@ -1,7 +1,11 @@
-﻿namespace TravelAppApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelAppApi.Models
 {
     public class Spot
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public double Rating { get; set; }
